3b49c81374499e195a96231587ec988ce8282cae
Hailin <hailin_qy@163.com>
2019-12-25 14:09:47 +0800

    refactor

---
+++ hl-echo/src/main/java/com/hl/echo/client.java
@@ -0,0 +1,15 @@
+package com.hl.echo;
+
+/**
+ * @author Hailin
+ * @date 2019/12/24
+ */
+public class client {
+
+    private static final String IP = "127.0.0.1";
+    private static final String PORT = "8888";
+
+    public static void main(String[] args) {
+
+    }
+}
